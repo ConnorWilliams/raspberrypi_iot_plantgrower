2496d0fd206ba9e551f5665cde03c74c60db1780
Connor Williams <connor_williams@msn.com>
2019-04-26 21:24:23 +0200

    Use logging library

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import logging
 import paho.mqtt.client as mqtt
+
+logger = logging.getLogger(__name__)
 
 
 class PlantgrowerMQTTClient(mqtt.Client):
@@ -10,26 +13,27 @@ class PlantgrowerMQTTClient(mqtt.Client):
         super().__init__(**kwargs)
 
     def on_connect(self, client, userdata, flags, rc):
-        print(f"Connection returned result: {rc}")
-        self.subscribe("grow/{self.grow_id}/#", 0)
+        logger.info(f"Connection returned result: {rc}")
+        self.subscribe(f"grow/{self.grow_id}/#", 0)
 
     def on_message(self, client, userdata, msg):
-        print(f"Unrecognized message received: {msg.topic} - {msg.payload}")
+        logger.info(f"Unrecognized message received: {msg.topic} - {msg.payload}")
 
     def on_publish(self, client, userdata, mid):
-        print("mid: "+str(mid))
+        logger.info("mid: "+str(mid))
 
     def on_subscribe(self, client, userdata, mid, granted_qos):
-        print("Subscribed: "+str(mid)+" "+str(granted_qos))
+        logger.info("Subscribed: "+str(mid)+" "+str(granted_qos))
 
     def on_log(self, client, userdata, level, string):
-        print(string)
+        logger.info(string)
 
-    def on_disconnect(client, userdata, rc):
+    def on_disconnect(self, client, userdata, rc):
         if rc != 0:
-            print("Unexpected disconnection.")
+            logger.info("Unexpected disconnection. Reconnecting.")
+            self.reconnect()
         else:
-            print("Received disconnect signal.")
+            logger.info("Received disconnect signal.")
 
     def message_callback_add(self, sub_filter, callback):
         super().message_callback_add(
--- raspi_consumer.py
+++ raspi_consumer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import logging
 import os
 from mqtt_client import PlantgrowerMQTTClient
+
+
+logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG)
 
 GROW_ID = os.getenv("GROW_ID", "1")
 MOSQUITTO_HOST = os.getenv("MOSQUITTO_HOST", "m2m.eclipse.org")
@@ -13,16 +18,15 @@ MOSQUITTO_KEEPALIVE = os.getenv("MOSQUITTO_KEEPALIVE", "60")
 def on_message_output(mosq, obj, msg):
     # This callback will only be called for messages with topics that match
     # grow/id/output/#
-    print(f"MESSAGE: {msg.topic} - {msg.payload}")
+    logger.info(f"MESSAGE: {msg.topic} - {msg.payload}")
 
 
-# TODO: Parametrise grow_id or use device serial?
 mqttc = PlantgrowerMQTTClient(grow_id=GROW_ID)
 
 mqttc.message_callback_add("output", on_message_output)
 # TODO: Add callback for sensor reading
 
-print(f"Connecting to {MOSQUITTO_HOST} on port {MOSQUITTO_PORT}")
+logger.info(f"Connecting to {MOSQUITTO_HOST} on port {MOSQUITTO_PORT}")
 mqttc.connect(MOSQUITTO_HOST, int(MOSQUITTO_PORT), int(MOSQUITTO_KEEPALIVE))
 
 mqttc.loop_forever()
