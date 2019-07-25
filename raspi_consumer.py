0fd1616968fe4f5d5d2ada03af9cc7de63525b00
Connor Williams <connor_williams@msn.com>
2019-07-25 18:05:35 +0200

    Publish sensor readings to new_reading topic

--- raspi_consumer.py
+++ raspi_consumer.py
@@ -7,6 +7,7 @@ import re
 import gpiozero
 from gpiozero.pins.mock import MockFactory
 from mqtt_client import PlantgrowerMQTTClient
+from paho.mqtt import publish
 from sensors import BME280
 
 
@@ -27,8 +28,12 @@ output_devices = {}
 
 
 def on_message_output(mosq, obj, msg):
-    # This callback will only be called for messages with topics that match
-    # grow/id/output/#
+    """
+    Callback for messages with topics that match
+    grow/id/output/# of the form ([11-27], True|False)
+
+    Validates the message, parses it and calls a function which does the I/O
+    """
     logger.info(f"Message on {msg.topic} on grow {GROW_ID}: {msg.payload}")
     message_string = validate_message(
         msg.payload,
@@ -41,22 +46,41 @@ def on_message_output(mosq, obj, msg):
 
 
 def on_message_read(mosq, obj, msg):
+    """
+    Callback for messages with topics that match
+    grow/id/read/# of the form (sensor_id, [11-27], sensor_model)
+
+    Validates the message, parses it and calls a function which does the read
+    Sends reading data to the new_reading mqtt topic
+    """
     # This callback will only be called for messages with topics that match
     # grow/id/read/#
     logger.info(f"Message on {msg.topic} on grow {GROW_ID}: {msg.payload}")
     message_string = validate_message(
         msg.payload,
-        r"^[(](1[1-9]|2[0-7]){1}, (.*)[)]$",  # ([11-27], sensor_type)
-        "(pin_number, sensor_type)"
+        r"^[(]([0-9]+), (1[1-9]|2[0-7]){1}, (.*)[)]$",  # (id, [11-27], sensor_model)
+        "(sensor_id, pin_number, sensor_model)"
     ).replace(" ", "")
 
-    pin_number, sensor_type = tuple(message_string[1:-1].split(","))
+    sensor_id, pin_number, sensor_type = tuple(message_string[1:-1].split(","))
     sensor_type = sensor_type.replace("'", "")
     reading = read_sensor(pin_number, sensor_type)
-    logger.info(reading)
+    for reading_category, value in reading['values'].items():
+        message = str((
+            sensor_id, reading_category, reading['timestamp'], value
+        ))
+        publish.single(
+            f"new_reading",
+            message,
+            hostname="mosquitto"
+        )
+        logger.info(f"Published to new_reading topic: {message}")
 
 
 def validate_message(message, regex_string, proper_form):
+    """
+    Validates that a message matches a regular expression
+    """
     regex = re.compile(regex_string)
     decoded_message = message.decode("utf-8")
     if regex.match(decoded_message):
@@ -67,6 +91,9 @@ def validate_message(message, regex_string, proper_form):
 
 
 def set_output_pin(pin_number, status):
+    """
+    Sets the pin status HIGH on True, LOW on False.
+    """
     if pin_number not in output_devices.keys():
         logger.info(f"Creating new gpio outputdevice on pin {pin_number}")
         output_devices[pin_number] = gpiozero.OutputDevice(
@@ -78,10 +105,15 @@ def set_output_pin(pin_number, status):
     output_device = output_devices[pin_number]
     output_device.on() if status == "True" \
         else output_device.off()
-    logger.debug(output_device)
 
 
 def read_sensor(pin_number, sensor_type):
+    """
+    Creates the sensor object an calls the read method
+    to read a sensor on a particular pin
+
+    Add new supported sensors to the dict
+    """
     sensor_type = sensor_type.lower()
     SUPPORTED_SENSORS = {
         'bme280': BME280
